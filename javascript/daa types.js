a15fa20b7d319ab83ef236d4a68bed137382fabe
muhammed-aadhil04 <muhammadaadhil2511@gmail.com>
2025-09-22 00:01:57 +0530

    task2

---
+++ javascript/daa types.js
@@ -0,0 +1,31 @@
+let color="yellow";
+let lastname="aadhil";
+
+let length=16;
+let weight=7.6;
+
+// let x=1244252767087378892n;
+// let y=bigint(1123466774896438)
+
+let x1=true;
+let y1=false;
+
+const person={firstname:"muhammed",lastname:"aadhil"};
+const cars=["bmw","volvo","saab"];
+const date=new Date ("2022-03-25");
+
+let x;
+let y;
+
+let x3=null;
+let x4=null;
+
+console.log("string:",color,lastname);
+console.log("number:",length,weight);
+// console.log("bigInt:",big1,big2);
+// console.log("booleans:",ishHappy,isSad);
+ console.log("object:",person);
+ console.log("array:",cars);
+ console.log("date:",date);
+// console.log("undefined:",notdefined1,notdefined2);
+// console.log("null:",emptyvalue1,emptyvalue2);
