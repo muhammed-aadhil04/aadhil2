1d545db8aceb66cb406cca0961e9bae9496718bd
muhammed-aadhil04 <muhammadaadhil2511@gmail.com>
2025-09-22 12:53:54 +0530

    submit

---
+++ javascript/variables.js
@@ -0,0 +1,21 @@
+var name="aadhil";
+let age="25";
+const pi=3.14
+
+
+
+ age=26
+
+//  datatypes
+ let str="hello";         // string 
+ let num="100";           // number
+ let ishappy=true;        //boolean
+ let empty=null;          //null
+ let notassigned;         //undefined
+ let person={name:"aadhil",age:25};   //object
+ let numbers=[1,2,3];     //array
+
+
+ console.log(str)
+ console.log(typeof(ishappy))
+
